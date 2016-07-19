830faa116352a587e6fc1889eda8cdcdb91db86c
Nikko Ortega <NFCOrtega89@gmail.com>
2016-07-19 14:44:18 -0700

    intial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+for (var i = 0; i <= 100; i++) {
+    var output = "";
+    if (i % 3 == 0)
+        output += "fizz";
+    if (i % 5 == 0)
+        output += "buzz";
+    console.log(output || i);
+}
